7f6b2ccc6bc3f3d7bd82be03d30d95a35fc0c572
jmusila <jonathanmusila6@gmail.com>
2023-05-11 12:20:25 +0300

    subscriber: add styles to landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-   <div class="flex flex-col bg-indigo-900 w-full h-screen">
+   <div class="flex flex-col bg-indigo-900 w-full h-screen" x-data="{showSubscribe: false,}">
     <nav class="flex pt-5 justify-between container mx-auto text-indigo-200">
         <a class="text-4xl font-bold" href="/"><x-application-logo class="w-16 h-16 fill-current"></x-application-logo></a>
         <div class="flex justify-end">
@@ -14,10 +14,10 @@
         <div class="flex flex-col w-1/3 items-start">
         <h1 class="text-white font-bold text-5xl leading-tight mb-4">Simple generic landing page to subscribe.</h1>
         <p class="text-indigo-200 text-xl mb-10">We are just learning the <span class="font-bold underline">TALL</span> stack. Would you mind subscribing?</p>
-        <x-primary-button class="py-3 px-8 bg-red-500 hover:bg-red-600">Subscribe</x-primary-button>
+        <x-primary-button class="py-3 px-8 bg-red-500 hover:bg-red-600" x-on:click="showSubscribe = true">Subscribe</x-primary-button>
         </div>
     </div>
-    <div class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center justify-center w-full h-full">
+    <div class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center justify-center w-full h-full" x-show="showSubscribe" x-on:click.self="showSubscribe = false">
         <div class="mg-auto bg-pink-500 shadow-2xl rounded-xl p-8">
             <p class="text-white text-5xl font-extrabold text-center">Let's do it!</p>
             <form class="flex flex-col items-center p-24"> 
