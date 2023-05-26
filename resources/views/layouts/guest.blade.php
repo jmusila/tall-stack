08728dd0dea935a0bf36b604689ab2f31f71601c
jmusila <jonathanmusila6@gmail.com>
2023-05-26 10:55:58 +0300

    updates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,6 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         @livewireStyles
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
