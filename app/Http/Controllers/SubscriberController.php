27ea7ec284e05a2f17ad0bdacce095ad4d4cca63
jmusila <jonathanmusila6@gmail.com>
2023-07-03 12:37:22 +0300

    all

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    public function verify(Subscriber $subscriber)
+    {
+        $subscriber->markEmailAsVerified();
+    }
 }
