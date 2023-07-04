5adc4938580448a4e9051326eb415db458ab2639
jmusila <jonathanmusila6@gmail.com>
2023-07-04 11:37:35 +0300

    routes: register dashboard routes

---
+++ routes/dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriberController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
